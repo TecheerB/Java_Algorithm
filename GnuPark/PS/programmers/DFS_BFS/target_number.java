7d62759b2d886fa07b867b90e1d79ee555c076d3
Gnu-Kenny <qkr0677@gmail.com>
2021-11-28 23:55:30 +0900

    [211128] 프로그래머스_타겟넘버

---
+++ GnuPark/PS/programmers/DFS_BFS/target_number.java
@@ -0,0 +1,26 @@
+class Solution {
+    static int answer;
+
+    public void dfs(int[] numbers, int target, int idx, int sum) {
+        if (idx == numbers.length) {
+            if (sum == target)
+                answer++;
+            return;
+        }
+
+        sum += numbers[idx];
+        dfs(numbers, target, idx + 1, sum);
+        sum -= numbers[idx];
+        sum += (-1 * numbers[idx]);
+        dfs(numbers, target, idx + 1, sum);
+
+    }
+
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+
+        dfs(numbers, target, 0, 0);
+
+        return answer;
+    }
+}
